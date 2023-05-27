4c6705cdbde0945133dc4280cb780d85fa2f116f
Daniel S <dmschopler@gmail.com>
2023-05-27 16:05:39 -0400

    Added server.js and created Port

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3001;
+const path = require('path');
+const routes = require('./routes/routes');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', routes);
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.listen(PORT, () =>
+    console.log(`Example app listening at http://localhost:${PORT}`)
+);
